18815dc1b22c7d8aac4ecb0d2f8711e7ae38b0d7
endrapranata <gedeendrapranata@gmail.com>
2023-08-07 11:57:47 +0800

    tes

---
+++ resources/views/endra.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Endra Blog</title>
+</head>
+<body>
+    <h1>My First Laravel Projects</h1>
+    <h1>Welcome to Fiber Optic Monitor</h1>
+
+    <?php
+    // Simulasikan data status koneksi
+    $connectionStatus = "Connected";
+    $downloadSpeed = 50;
+    $uploadSpeed = 20;
+    $categori = "Wired";
+
+    // Tampilkan informasi pemantauan
+    echo "<p>Status Koneksi: $connectionStatus</p>";
+    echo "<p>Download Speed: $downloadSpeed Mbps</p>";
+    echo "<p>Upload Speed: $uploadSpeed Mbps</p>";
+    echo "<p>Categori Internet:    $categori Connection</p>";
+    ?>
+</body>
+</html>
