ddfe86fcadce18bb2d0c4922d1281f29af6606f2
ardanayatraa <141602961+ardanayatraa@users.noreply.github.com>
2023-08-07 15:43:48 +0800

    Ubah tampilan login bang

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -16,20 +16,36 @@
 </head>
 
 <body>
-<form action="ngide" method="POST">
-    <label for="username">username  : </label>
-    <input type="text" id="username" name="username">
-    <br>
-    <label for="pass">password  :</label>
-    <input type="password" id="pass" name="password">
-    <br>
-    <button><input type="submit"></button>
-</form>
+    <div class="login-container">
+        <div class="fade-overlay">
+            <div class="card login-card">
+                <div class="card-body">
+                    <div class="text-center mb-4 mt-3">
+                        <img src="/img/logo.jpg" alt="User Icon" width="80">
+                    </div>
+                    <form action="#" method="POST">
+                        <div class="mb-3">
+                            <label for="username" class="form-label">Username:</label>
+                            <input type="text" class="form-control form-control-lg" id="username" name="username">
+                        </div>
+                        <div class="mb-3">
+                            <label for="pass" class="form-label">Password:</label>
+                            <input type="password" class="form-control form-control-lg" id="pass" name="password">
+                        </div>
+                        <div class="d-grid gap-1">
+                            <button type="submit" class="btn btn-success btn-lg btn-block">Login</button>
+                            <small class=" text-sm-center mt-3 mb-2 text-success">Ngide.net &copy; 2023</small>
+                            <a href="#" class="already">Belum punya akun ? Daftar.</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
 
- 
-    <script src="assets/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
-    </script>
+
+        <script src="assets/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+        </script>
 </body>
 
 </html>
