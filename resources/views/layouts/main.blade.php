d6938004e5a685f2367f52b12fbc00af889fbfbb
ardanayatraa <141602961+ardanayatraa@users.noreply.github.com>
2023-08-11 13:15:01 +0800

    fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,44 +14,44 @@
 </head>
 
 <body>
-    <!-- Navbar Atas -->
-    <nav class="navbar navbar-expand-lg bg-dark navbar-dark fixed-navbar">
-        <div class="container d-flex justify-content-between align-items-center">
-            <a class="navbar-brand" href="/">Ngide.net</a>
-            <div class="dropdown ml-5">
-                <button class="btn btn-secondary dropdown-toggle bg-transparent border-dark" type="button"
-                    id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="/img/avatar.jpg" alt="Avatar" class="avatar-image">
-                    <span class="ms-2 text-light">{{ Auth::user()->nama_lengkap }}</span>
-                </button>
-                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
-                    <!-- Menu Profil -->
-                    <li>
-                        <a class="dropdown-item" href="#" style="background-color: transparent;">
-                            <i style="margin-right: 5px;" class="fa-solid fa-person"></i> Rincian Profil
-                        </a>
-                    </li>
-                    <!-- Menu Logout -->
-                    <li><a class="dropdown-item" href="/logout" style="background-color: transparent;">
-                            <i style="margin-right: 5px;" class="fa-solid fa-right-to-bracket"></i>
-                            Logout</a></li>
-                </ul>
+    @if (Auth::check())
+        <!-- Navbar Atas -->
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark fixed-navbar">
+            <div class="container d-flex justify-content-between align-items-center">
+                <a class="navbar-brand" href="/">Ngide.net</a>
+                <div class="dropdown ml-5">
+                    <button class="btn btn-secondary dropdown-toggle bg-transparent border-dark" type="button"
+                        id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="/img/avatar.jpg" alt="Avatar" class="avatar-image">
+                        <span class="ms-2 text-light">{{ Auth::user()->nama_lengkap }}</span>
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+                        <!-- Menu Profil -->
+                        <li>
+                            <a class="dropdown-item" href="#" style="background-color: transparent;">
+                                <i style="margin-right: 5px;" class="fas fa-user"></i> Rincian Profil
+                            </a>
+                        </li>
+                        <!-- Menu Logout -->
+                        <li><a class="dropdown-item" href="/logout" style="background-color: transparent;">
+                                <i style="margin-right: 5px;" class="fas fa-sign-out-alt"></i> Logout</a></li>
+                    </ul>
+                </div>
             </div>
+        </nav>
+
+        <!-- Sidebar -->
+        <div class="sidebar">
+            <a href="#"><i class="fas fa-layer-group"></i> User List</a>
+            <a href="#"><i class="fas fa-link"></i> Manage Post</a>
+            <a href="#"><i class="fas fa-thumbtack"></i> Category</a>
         </div>
-    </nav>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-        <a href="#"><i class="fa-solid fa-layer-group"></i> User List</a>
-        <a href="#"><i class="fa-solid fa-link"></i> Manage Post</a>
-        <a href="#"><i class="fa-solid fa-thumbtack"></i> Category</a>
-
+        <!-- Konten Utama -->
+        <div class="main-content">
+    @endif
+    @yield('content')
     </div>
-
-    <!-- Konten Utama -->
-    <div class="main-content">
-        @yield('content') </div>
-
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
